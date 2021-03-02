ac9cd26363264e2d71af7cbcf9f925ea13597c8e
JOSE JOAQUIN SANCHEZ <info@sfsolutions.es>
2021-03-02 21:09:37 +0100

    Tercera versión!

--- Despliegue/src/despliegue/HolaMundo.java
+++ Despliegue/src/despliegue/HolaMundo.java
@@ -1,8 +1,14 @@
 package despliegue;
 
-public class HolaMundo {
+public class HolaMundo 
+{
 	public static String getSaluda()
 	{
 		return "Hola Mundo!";
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Tercera versión!");
 	}
 }
